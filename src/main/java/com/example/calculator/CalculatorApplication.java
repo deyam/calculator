51a4e9c85ba5c3fb923727de5e52ea64c7e50175
Deya Motawie <deya@b789.local>
2022-08-14 16:29:46 +0200

    added hazelcast yaml file

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -2,11 +2,17 @@ package com.example.calculator;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableCaching
@@ -29,4 +35,26 @@ public class CalculatorApplication {
 		return clientConfig;
 
 }
+
+
+//	@Bean
+//	public CacheManager cacheManager() {
+//		SimpleCacheManager cacheManager = new SimpleCacheManager();
+//		cacheManager.setCaches(Arrays.asList(
+////				new ConcurrentMapCache("directory"),
+//				new ConcurrentMapCache("sum")));
+//		return cacheManager;
+//	}
+
+	@Bean
+	Config config() {
+		Config config = new Config();
+		config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
+		config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+		config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+				.setProperty("namespace", "default")
+				.setProperty("service-name", "hazelcast");
+		return config;
+	}
+
 }
