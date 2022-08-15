3cebb2e115df64251ef273d5b96da69b2451fc76
Deya Motawie <deya@b789.local>
2022-08-15 17:48:11 +0200

    PollSCM3

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
-#!/bin/bash
 set -x
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
 NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
